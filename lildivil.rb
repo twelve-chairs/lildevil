017e61cb0966465b7d3bc84557dfa4c98376d8b0
Twelve Chairs Software, LLC <asimakov@twelvechairs-software.com>
2020-01-03 03:14:05 +0000

    Update lildivil.rb

--- lildivil.rb
+++ lildivil.rb
@@ -1,6 +1,7 @@
 class LilDevil < Formula
   desc "The Dev Tools Dashboard"
   homepage "https://gitlab.com/twelvechairssoftware/lildevil"
+  version "0.0.1"
 
 
   url "https://gitlab.com/twelvechairssoftware/lildevil/bin/darwin", :using => :curl

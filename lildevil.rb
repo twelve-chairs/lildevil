e16d5dc17ce7a2182be1159f449ddcb51a8bc601
Twelve Chairs Software, LLC <asimakov@twelvechairs-software.com>
2020-01-03 23:56:42 +0000

    Update lildevil.rb

--- lildevil.rb
+++ lildevil.rb
@@ -13,22 +13,10 @@ class Lildevil < Formula
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
-    system "ls", "-la"
+    system "ls", "la"
     system "unzip", "lildevil.zip"
     system "cp", "lilDevil.app", "~/Applications"
     # system "cmake", ".", *std_cmake_args
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test lildevil`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end

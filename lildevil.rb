8e4d74b9822aa02a7da5645e768b9f1c9d15e562
Twelve Chairs Software, LLC <asimakov@twelvechairs-software.com>
2020-01-03 17:53:24 +0000

    Update lildevil.rb

--- lildevil.rb
+++ lildevil.rb
@@ -2,7 +2,7 @@ cask 'lildevil' do
   version ':latest'
   sha256 'b5511bb9b2ac51de62d04ea96186dcc21124804db66cf3fef76385706db4deef'
 
-  url "https://gitlab.com/twelvechairssoftware/lildevil/lildevil.zip"
+  url "https://gitlab.com/twelvechairssoftware/lildevil/raw/master/dist/homebrew/lildevil.zip"
   name 'lildevil'
   homepage 'https://gitlab.com/twelvechairssoftware/lildevil'
 

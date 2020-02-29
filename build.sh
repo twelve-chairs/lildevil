f6d518b2de1ebde9533c8056a4b11cadb3cd78b6
Twelve Chairs Software, LLC <asimakov@twelvechairs-software.com>
2020-02-29 16:02:17 -0500

    homebrew

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-#sudo dnf groupinstall "Development Tools"
-#
-#sudo dnf install gcc-c++ cmake qt5
+dnf groupinstall "Development Tools"
+
+dnf install gcc-c++ cmake qt5
+
+source ~/.bashrc
+
+mkdir cmake-build-minsizerel
 
 cmake -DCMAKE_BUILD_TYPE=MinSizeRel -G "CodeBlocks - Unix Makefiles" .
 

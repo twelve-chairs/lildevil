aba19c91bcb52eec225ad3e4b8fd37a77aea7753
Twelve Chairs Software, LLC <asimakov@twelvechairs-software.com>
2020-02-29 15:28:36 -0500

    homebrew

---
+++ build.sh
@@ -0,0 +1,7 @@
+dnf groupinstall "Development Tools"
+
+dnf install gcc-c++ cmake qt5
+
+cmake -DCMAKE_BUILD_TYPE=MinSizeRel -G "CodeBlocks - Unix Makefiles" .
+
+cmake --build ./cmake-build-minsizerel --target lilDevil -- -j 2

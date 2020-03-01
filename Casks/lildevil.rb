353ea9a57c8e8a3923a58c938b8bd1d26e845c2d
Twelve Chairs Software, LLC <asimakov@twelvechairs-software.com>
2020-03-01 22:14:53 +0000

    Update lildevil.rb

--- Casks/lildevil.rb
+++ Casks/lildevil.rb
@@ -2,9 +2,9 @@ cask 'lildevil' do
   version ':latest'
   sha256 :no_check
 
-  url "https://gitlab.com/twelvechairssoftware/lildevil/-/raw/a23b274df64d348ab1def821d1ff28e07f1cd77a/dist/homebrew/lildevil.zip?inline=false"
+  url "https://github.com/twelvechairssoftware/lildevil/raw/master/dist/homebrew/lildevil.zip"
   name 'lilDevil'
-  homepage 'https://gitlab.com/twelvechairssoftware/lildevil'
+  homepage 'https://github.com/twelvechairssoftware/lildevil'
 
   app 'lilDevil.app'
 end

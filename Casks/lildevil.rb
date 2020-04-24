f82b44de80b8c77a5ad2d6d1be32cc0f5baeacca
Twelve Chairs Software, LLC <asimakov@twelvechairs-software.com>
2020-04-23 22:35:50 -0400

    Apple notarization setup

--- Casks/lildevil.rb
+++ Casks/lildevil.rb
@@ -2,7 +2,7 @@ cask 'lildevil' do
   version ':latest'
   sha256 :no_check
 
-  url "https://github.com/twelvechairssoftware/lildevil/raw/master/dist/homebrew/lildevil.zip"
+  url "https://github.com/twelvechairssoftware/lildevil/raw/master/dist/homebrew/lildevil.pkg"
   name 'lilDevil'
   homepage 'https://github.com/twelvechairssoftware/lildevil'
 
